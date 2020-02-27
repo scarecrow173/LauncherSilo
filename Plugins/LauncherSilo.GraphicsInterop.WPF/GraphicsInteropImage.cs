e9edd4d4b227ec802e4038191c03d0339a6e5e23
scarecrow173 <akiyoshi.koyama@gmail.com>
2020-02-27 16:47:05 +0900

    update GraphicsInterop.WPF. add GraphicsInteropImage

---
+++ Plugins/LauncherSilo.GraphicsInterop.WPF/GraphicsInteropImage.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace LauncherSilo.GraphicsInterop.WPF
+{
+    public class GraphicsInteropImage : System.Windows.Controls.Image
+    {
+        public bool IsInDesignMode
+        {
+            get
+            {
+                var prop = DesignerProperties.IsInDesignModeProperty;
+                var isDesignMode = (bool)DependencyPropertyDescriptor.FromProperty(prop, typeof(FrameworkElement)).Metadata.DefaultValue;
+                return isDesignMode;
+            }
+        }
+        private OffscreenRenderFrame renderFrame = new OffscreenRenderFrame();
+        private GraphicsInteropImageSource imageSource = new GraphicsInteropImageSource();
+
+        public GraphicsInteropImage()
+        {
+            Loaded += GraphicsInteropImage_Loaded;
+            Unloaded += GraphicsInteropImage_Unloaded;
+            Stretch = System.Windows.Media.Stretch.Fill;
+        }
+
+        private void GraphicsInteropImage_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (IsInDesignMode)
+            {
+                return;
+            }
+            Dispatcher.BeginInvoke(new Action(() =>
+            {
+                renderFrame.Height = (int)ActualHeight;
+                renderFrame.Width = (int)ActualWidth;
+                renderFrame.RenderTargetChanged += RenderFrame_RenderTargetChanged; ;
+                renderFrame.Initialize();
+                Source = imageSource;
+                SizeChanged += GraphicsInteropImage_SizeChanged;
+
+            }));
+        }
+
+        private void GraphicsInteropImage_Unloaded(object sender, RoutedEventArgs e)
+        {
+            if (IsInDesignMode)
+            {
+                return;
+            }
+        }
+
+        private void RenderFrame_RenderTargetChanged(object sender, RenderTargetChanageArgs e)
+        {
+            imageSource.SetSurface(e.RenderTargetTexture);
+        }
+        private void GraphicsInteropImage_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            renderFrame.Height = (int)ActualHeight;
+            renderFrame.Width = (int)ActualWidth;
+        }
+    }
+}
